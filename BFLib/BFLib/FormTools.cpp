6f0154e037fb73bde64861eb1b1a982e97df3c46
jiiks <jiiks@windowslive.com>
2014-02-12 15:26:21 +0200

    moveForm method

--- BFLib/BFLib/FormTools.cpp
+++ BFLib/BFLib/FormTools.cpp
@@ -2,6 +2,10 @@
 
 extern "C"
 {
+	//Used to move a borderless form
+	//Usage:
+	//[DllImport("BFLib.dll")]
+	//public static extern void moveForm(IntPtr hWnd = form handle);
 	__declspec(dllexport) void __stdcall moveForm(HWND hWnd)
 	{
 		if (GetKeyState(VK_LBUTTON) < 0)
